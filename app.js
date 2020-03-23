3d4474150699ae301de737ddb427d9a37ae01ce9
Luiz Fernando D <lfluiz.lf@gmail.com>
2020-03-23 19:28:13 -0300

    Alguns problemas relacionados ao backend foram arrumados. Foram adicionados mais perguntas.

--- app.js
+++ app.js
@@ -9,21 +9,62 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public")); //deixar um diretorio publico para acesso das urls
 
 const dir = __dirname + "/views/"
+const wrongPage = dir + "wrong.html";
 
+/* PROTOCOLOS GETS */
 app.get("/", (req,res) => {
 
     res.sendFile(dir + "index.html");
     //console.log(req.body);
     
-
 });
 
+app.get("/q1", (req,res) => {
+    res.sendFile(dir + "question1.html");
+});
+
+app.get("/q2", (req,res) => {
+    res.sendFile(dir + "question2.html");
+});
+
+
+
+
+/* PROTOCOLOS POSTS */
 app.post("/", (req,res) => {
-    let resposta = req.body;
+    const resposta = req.body.resp;
     console.log(resposta);
-    res.send("parabens!");
+    if (resposta == "petrus" || resposta == "Petrus") {
+        res.redirect("/q1");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
     
-})
+});
+
+app.post("/q1", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "Dxf7" || resposta == "Dxf7#") {
+        res.redirect("/q2");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
+});
+
+app.post("/q2", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "makesureyouwanttocontinue") {
+        res.redirect("/");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
+    
+});
 
 
 app.listen(3000, () => {
