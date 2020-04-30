fbc07d0012b41384bd31928fd4cfab9026426950
Luiz Fernando D <lfluiz.lf@gmail.com>
2020-04-30 18:59:29 -0300

    New Questions

--- app.js
+++ app.js
@@ -14,20 +14,29 @@ const wrongPage = dir + "wrong.html";
 
 /* PROTOCOLOS GETS */
 app.get("/", (req,res) => {
-
-    res.sendFile(dir + "index.html");
-    //console.log(req.body);
-    
+    res.sendFile(dir + "index.html"); 
 });
-
 app.get("/q1", (req,res) => {
     res.sendFile(dir + "question1.html");
 });
-
 app.get("/q2", (req,res) => {
     res.sendFile(dir + "question2.html");
 });
-
+app.get("/q3", (req,res) => {
+    res.sendFile(dir + "question3.html");
+});
+app.get("/q4", (req,res) => {
+    res.sendFile(dir + "question4.html");
+});
+app.get("/q5", (req,res) => {
+    res.sendFile(dir + "question5.html");
+});
+app.get("/q6", (req,res) => {
+    res.sendFile(dir + "question6.html");
+});
+app.get("/q7", (req,res) => {
+    res.sendFile(dir + "question7.html");
+});
 
 
 
@@ -41,7 +50,6 @@ app.post("/", (req,res) => {
     else {
         res.sendFile(wrongPage);
     }
-    
 });
 
 app.post("/q1", (req,res) => {
@@ -59,12 +67,66 @@ app.post("/q2", (req,res) => {
     const resposta = req.body.resp;
     console.log(resposta);
     if (resposta == "makesureyouwanttocontinue") {
-        res.redirect("/");
+        res.redirect("/q3");
+    }
+    else {
+        res.sendFile(wrongPage);
+    } 
+});
+
+app.post("/q3", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "wuhan") {
+        res.redirect("/q4");
     }
     else {
         res.sendFile(wrongPage);
     }
-    
+});
+
+app.post("/q4", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "richard burr") {
+        res.redirect("/q5");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
+});
+
+app.post("/q5", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "tedros") {
+        res.redirect("/q6");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
+});
+
+app.post("/q6", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "tedros") {
+        res.redirect("/q7");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
+});
+
+app.post("/q7", (req,res) => {
+    const resposta = req.body.resp;
+    console.log(resposta);
+    if (resposta == "tedros") {
+        res.redirect("/q8");
+    }
+    else {
+        res.sendFile(wrongPage);
+    }
 });
 
 
